3c1a8fd3bf732e7e919e4ec1a426587f7c159a28
Alexander Zakharchenko <zaharay@yandex.ru>
2021-04-30 15:08:45 +0300

    add class PostgresWrapper

--- db_mirror.py
+++ db_mirror.py
@@ -1,7 +1,58 @@
 # (venv)>pip install SQLAlchemy
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, Float, String, DateTime
 
-engine = create_engine('postgresql://admin:zahar@localhost:5432/mirror', echo=True)
-meta = MetaData()
+types_dict = {
+    'C': String,    # CHAR, CUKY
+    'N': Integer,   # NUMC
+    'P': Float,     # CURR
+}
 
 
+def replace_list_by_dict(my_list, my_dict):
+    for idx, val in enumerate(my_list):
+        if val in list(my_dict.keys()):
+            my_list[idx] = my_dict[val]
+        else:
+            print('Err!')
+    return my_list
+
+
+class PostgresWrapper:
+    def __init__(self, url):
+        self.url = url
+        self.connection = None
+
+    def connect(self):
+        engine = create_engine(self.url, echo=True)
+        self.connection = engine.connect()
+
+    def create_table_from_metadata_df(self, name, metadata):
+        try:
+            columns_names = list(metadata['FIELDNAME'])
+            columns_types = list(metadata['INTTYPE'])
+            for idx, val in enumerate(columns_types):
+                if val in list(types_dict.keys()):
+                    columns_types[idx] = types_dict[val]
+                else:
+                    print('При создании таблицы "{0}" обнаружен неизвестный тип данных: "{1}"!',
+                          'Таблица не создана!', sep='\n')
+                    return
+            print(columns_types)
+            # primary_key_flags = [True, False, False, False]
+            # nullable_flags = [False, False, False, False]
+            #
+            # meta = MetaData()
+            # table = Table(name, meta,
+            #               *(Column(column_name, column_type,
+            #                        primary_key=primary_key_flag,
+            #                        nullable=nullable_flag)
+            #                 for column_name,
+            #                     column_type,
+            #                     primary_key_flag,
+            #                     nullable_flag in zip(columns_names,
+            #                                          columns_types,
+            #                                          primary_key_flags,
+            #                                          nullable_flags)))
+
+        except Exception as ex:
+            print('Исключение при создании таблицы "{0}" в БД: {1}'.format(self.name, ex))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from json_proc import Datamart, get_datamarts_list
+from db_mirror import PostgresWrapper
 
 # path = 'c:/Users/admin/PycharmProjects/json_parser/data/'
 # path = 'd:/Git/Python/json_parser/data'
@@ -7,7 +8,7 @@ datamarts_path = r'c:/Users/admin/PycharmProjects/json_parser/data/'
 datamarts_file = r'datamarts.json'
 metadata_path = r'c:/Users/admin/PycharmProjects/json_parser/data/'
 data_path = r'c:/Users/admin/PycharmProjects/json_parser/data/'
-
+db_url = 'postgresql://postgres:zahar@localhost:5432/mirror'
 
 def write_csv(filename, data):
     """Запись данных в CSV-файл"""
@@ -18,19 +19,29 @@ def write_csv(filename, data):
     else:
         print('Отсутствуют данные для загрузки в файл {}!'.format(filename))
 
-
 def main():
+    replace(li, data_types)
+    print(li)
+
     # Получение списка витрин:
     datamarts_list = get_datamarts_list(os.path.join(datamarts_path, datamarts_file))
     print('-'*50, 'Список витрин:', datamarts_list, sep='\n')
 
+    dm_name = datamarts_list[0].split('/')[-1]
+
     # Экземпляр витрины
-    dm1 = Datamart(metadata_path, data_path, 'AIRSHINYL00')
+    dm1 = Datamart(metadata_path, data_path, dm_name)
     dm1_metadata = dm1.get_metadata_as_df()
     dm1_data = dm1.get_data_as_df()
 
     # write_csv('metadata_{}.csv'.format(dm1.name), dm1_metadata)
     # write_csv('data_{}.csv'.format(dm1.name), dm1_data)
+    #
+    # pw = PostgresWrapper(db_url)
+    # pw.create_table_from_metadata_df(dm_name, dm1_metadata)
+    # pw.connect()
+    # print(pw.connection)
+
 
 
 if __name__ == "__main__":
