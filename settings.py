bc9e4a44864de08bd159ab677f9e57919ba06043
Alexander Zakharchenko <zaharay@yandex.ru>
2021-05-11 16:44:47 +0300

    add logger

---
+++ settings.py
@@ -0,0 +1,53 @@
+"""
+Логгер
+"""
+
+import logging
+
+
+class MegaHandler(logging.Handler):
+    def __init__(self, filename):
+        logging.Handler.__init__(self)
+        self.filename = filename
+
+    def emit(self, record):
+        message = self.format(record)
+        with open(self.filename, 'a') as file:
+            file.write(message + '\n')
+
+
+logger_config = {
+    'version': 1,
+    'disable_existing_loggers': False, # отключение остальных логгеров
+
+    'formatters': {  # форматировщики
+        'std_format': {
+            'format': '{asctime} - {levelname} - {name} - {module}:{funcName}:{lineno} - {message}',
+            'style': '{'
+        }
+    },
+    'handlers': {  # обработчики
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'std_format'
+        },
+        'file': {
+            '()': MegaHandler,  # экземпляр MegaHandler
+            'level': 'DEBUG',
+            'filename': 'debug.log',
+            'formatter': 'std_format'
+        }
+    },
+    'loggers': {  # логгеры
+        'app_logger': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file']
+            #'propagate': False
+        }
+    },
+
+    # 'filters': {},
+    # 'root': {}   # '': {}
+    # 'incremental': True
+}
